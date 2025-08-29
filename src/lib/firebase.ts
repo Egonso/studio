c96798bfa4ac04ae7e8280d67d8e45f633e0e87c
Zoltan Gal <mail.zoltangal@gmail.com>
2025-08-29 13:58:02 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : get_app();
 const auth = getAuth(app);
 
 export {app, auth};
